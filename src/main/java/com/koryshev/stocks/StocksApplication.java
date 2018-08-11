8a3b22ee8fdb018115f771f770a240d5f748e491
ivan.koryshev <ivan.koryshev@dsr-company.com>
2018-08-11 18:28:52 +0300

    Add an app skeleton

---
+++ src/main/java/com/koryshev/stocks/StocksApplication.java
@@ -0,0 +1,17 @@
+package com.koryshev.stocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Starts the Stocks application.
+ *
+ * @author Ivan Koryshev
+ */
+@SpringBootApplication
+public class StocksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StocksApplication.class, args);
+    }
+}
