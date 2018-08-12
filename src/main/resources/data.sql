6cab29fd77f28deac607bc265458e6d22d84a799
ivan.koryshev <ivan.koryshev@dsr-company.com>
2018-08-12 12:42:59 +0300

    Populate data into DB on startup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into stock (id, name, current_price, created_date, last_update) values
+  (1, 'Amazon.com, Inc.', 1609.08, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+  (2, 'Apple Inc.', 190.04, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+  (3, 'Facebook, Inc.', 185.53, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+  (4, 'NVIDIA Corporation', 260.13, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+  (5, 'Netflix, Inc.', 329.60, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+  (6, 'Alibaba Group Holding Limited', 195.96, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+  (7, 'Microsoft Corporation', 97.91, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+  (8, 'Tesla, Inc.', 305.02, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+  (9, 'Intel Corporation', 54.98, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+  (10, 'Twitter, Inc.', 32.87, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
